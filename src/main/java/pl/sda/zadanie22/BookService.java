e7cf207438ffd413d9b730faa1ff3f5803e99999
rDk107c <radoslaw.kowalik90@gmail.com>
2021-08-23 19:57:35 +0200

    Add and Remove added

--- src/main/java/pl/sda/zadanie22/BookService.java
+++ src/main/java/pl/sda/zadanie22/BookService.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class BookService {
 
     private List<Book> listOfBooks = new ArrayList<>();
-    
+
+    public void addBook (Book book){
+        listOfBooks.add(book);
+    }
+    public void removeBook (Book book){
+        listOfBooks.remove(book);
+    }
+    public List<Book> listOfBooks (){
+        return listOfBooks;
+    }
 
 }
